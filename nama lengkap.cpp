5b8db626ead5107a4c0b4f6da224d40b404915cc
Muhammad Sandi R. Saepudin <muhammad.sandi.r.saepudin-2021@ftmm.unair.ac.id>
2022-11-08 01:28:19 +0700

    nyoba

---
+++ nama lengkap.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main (){
+    string nama;
+    cout << "Masukkan nama: ";
+    getline(cin, nama);
+    cout << "Nama anda adalah " << nama << endl;
+    return 0;
+}
